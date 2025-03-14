e08b6f7e99b9a4987dc04496fe3a9872b2ccb84f
Pham Hoang Ha <phamhoangha14062006@gmail.com>
2025-03-14 17:14:02 +0700

    thuc tap phan tich kieu du lieu

--- thực tập phân tích kiểu dữ liệu/job-listing-template/app.js
+++ thực tập phân tích kiểu dữ liệu/job-listing-template/app.js
@@ -224,100 +224,186 @@ const REQUEST = [
   },
 ];
 let elJobList = document.getElementById('jobList');
-let html = '';
+let filterJobTypes = document.getElementById('filterJobTypes');
+let nameCompany = document.getElementById('nameCompany')
+let filterWorkTypes = document.getElementById('filterWorkTypes');
+let technologyTypes = document.getElementById('technologyTypes');
+let inputSearch = document.getElementById('inputSearch');
+let btnSearch = document.getElementById('btnSearch');
+
+renderFilterArea();
+
+// render jobs
+
+renderList(JOBS);
+
+
+
+
+
 function showRequestItem(content) {
   return /*html*/`<a href="#" class="badge badge-outline text-secondary fw-normal badge-pill">${content}</a> `
 }
-JOBS.forEach(jobItem => {
-  const workTypeId = jobItem.workType;
-  const workType = WORK_TYPE.find((workTypeItem) => {
-    return workTypeItem.id == workTypeId
+
+function renderFilterArea() {
+  // render filter category
+  let htmljobType = '';
+  CATEGORY.forEach(jobTypeItem => {
+    htmljobType +=/*html*/`
+    <label class="form-check">
+      <input type="checkbox" class="form-check-input" name="form-type[]" value="${jobTypeItem.id}" />
+      <span class="form-check-label">${jobTypeItem.name}</span>
+    </label>`;
   })
-  const categoryId = jobItem.categoryId;
-  const category = CATEGORY.find((categoryItem) => {
-    return categoryItem.id == categoryId
+  filterJobTypes.innerHTML = htmljobType
+
+  // render filter company
+  let htmlCompany = '';
+  COMPANY.forEach(companyItem => {
+    htmlCompany +=/*html*/`
+    <label class="form-check">
+      <input type="checkbox" class="form-check-input" name="form-type[]" value="${companyItem.id}" />
+      <span class="form-check-label">${companyItem.name}</span>
+    </label>`;
   })
-  const companyId = jobItem.companyId;
-  const company = COMPANY.find((companyItem) => {
-    return companyItem.id == companyId
+  nameCompany.innerHTML = htmlCompany
+
+  // render filter worktype
+  let htmlWorkType = '';
+  WORK_TYPE.forEach(workTypeItem => {
+    htmlWorkType +=/*html*/`
+    <label class="form-check">
+      <input type="checkbox" class="form-check-input" name="form-type[]" value="${workTypeItem.id}" />
+      <span class="form-check-label">${workTypeItem.name}</span>
+    </label>`;
   })
-  const requestIds = jobItem.request;
-  let htmlRequest = '';
-  if (requestIds.length > 3) {
-    for (let i = 0; i < 3; i++) {
-      const currentRequestInfo = REQUEST.find(requestItem => requestItem.id == requestIds[i]);
-      htmlRequest += showRequestItem(currentRequestInfo.name);
+  filterWorkTypes.innerHTML = htmlWorkType
+
+  // render filter technology type
+  let htmlTechnologyType = '';
+  REQUEST.forEach(technologyItem => {
+    htmlTechnologyType +=/*html*/`
+    <label class="form-check">
+      <input type="checkbox" class="form-check-input" name="form-type[]" value="${technologyItem.id}" />
+      <span class="form-check-label">${technologyItem.name}</span>
+    </label>`;
+  })
+  technologyTypes.innerHTML = htmlTechnologyType
+}
+
+function renderList(list) {
+  let html = '';
+  list.forEach(jobItem => {
+    const workTypeId = jobItem.workType;
+    const workType = WORK_TYPE.find((workTypeItem) => {
+      return workTypeItem.id == workTypeId
+    })
+    const categoryId = jobItem.categoryId;
+    const category = CATEGORY.find((categoryItem) => {
+      return categoryItem.id == categoryId
+    })
+    const companyId = jobItem.companyId;
+    const company = COMPANY.find((companyItem) => {
+      return companyItem.id == companyId
+    })
+    const requestIds = jobItem.request;
+    let htmlRequest = '';
+    if (requestIds.length > 3) {
+      for (let i = 0; i < 3; i++) {
+        const currentRequestInfo = REQUEST.find(requestItem => requestItem.id == requestIds[i]);
+        htmlRequest += showRequestItem(currentRequestInfo.name);
+      }
+      const remainingRequest = `+${requestIds.length - 3}`;
+      htmlRequest += showRequestItem(remainingRequest);
+    } else {
+      requestIds.forEach(requestId => {
+        const currentRequestInfo = REQUEST.find(requestItem => requestItem.id == requestId);
+        htmlRequest += showRequestItem(currentRequestInfo.name);
+      });
     }
-    const remainingRequest = `+${requestIds.length - 3}`;
-    htmlRequest += showRequestItem(remainingRequest);
-  } else {
-    requestIds.forEach(requestId => {
-      const currentRequestInfo = REQUEST.find(requestItem => requestItem.id == requestId);
-      htmlRequest += showRequestItem(currentRequestInfo.name);
+    html += /*html*/`
+    <div class="card">
+      <div class="card-body">
+        <div class="row">
+          <div class="col">
+            <h3 class="mb-0">${jobItem.name}</h3>
+          </div>
+          <div class="col-auto fs-3 text-green">${jobItem.salaryMin}tr - ${jobItem.salaryMax}tr</div>
+        </div>
+        <div class="row">
+        <div class="col-md">
+        <span>${company.name}</span>
+        <div class="mt-3 list-inline list-inline-dots mb-0 text-secondary d-sm-block d-none">
+              <div class="list-inline-item">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                  fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                  stroke-linejoin="round" class="icon icon-inline icon-2">
+                  <path
+                    d="M8 9l5 5v7h-5v-4m0 4h-5v-7l5 -5m1 1v-6a1 1 0 0 1 1 -1h10a1 1 0 0 1 1 1v17h-8" />
+                  <path d="M13 7l0 .01" />
+                  <path d="M17 7l0 .01" />
+                  <path d="M17 11l0 .01" />
+                  <path d="M17 15l0 .01" />
+                </svg>
+                ${category.name}
+              </div>
+              <div class="list-inline-item">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                  fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                  stroke-linejoin="round" class="icon icon-inline icon-2">
+                  <path
+                    d="M15 21h-9a3 3 0 0 1 -3 -3v-1h10v2a2 2 0 0 0 4 0v-14a2 2 0 1 1 2 2h-2m2 -4h-11a3 3 0 0 0 -3 3v11" />
+                  <path d="M9 7l4 0" />
+                  <path d="M9 11l4 0" />
+                </svg>
+                ${workType.name}
+              </div>
+              <div class="list-inline-item">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                  fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                  stroke-linejoin="round" class="icon icon-inline icon-2">
+                  <path d="M9 11a3 3 0 1 0 6 0a3 3 0 0 0 -6 0" />
+                  <path
+                    d="M17.657 16.657l-4.243 4.243a2 2 0 0 1 -2.827 0l-4.244 -4.243a8 8 0 1 1 11.314 0z" />
+                </svg>
+                ${jobItem.address}
+              </div>
+            </div>
+          </div>
+          <div class="col-md-auto">
+            <div class="mt-3 badges">
+              ${htmlRequest}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>`;
+    elJobList.innerHTML = html;
+  });
+}
+
+btnSearch.addEventListener('click', function searchInput() {
+  let search = inputSearch.value.trim().toLowerCase();
+  let checkCategory = document.querySelectorAll('#filterJobTypes input');
+  let category = [];
+
+  checkCategory.forEach((item) => {
+    if (item.checked) {
+      category.push(parseInt(item.value));
+    }
+  });
+
+  let arrSearch = JOBS.filter(function (jobItem) {
+    return jobItem.name.toLowerCase().includes(search);
+  });
+  if (category.length > 0) {
+    arrSearch = arrSearch.filter(function (jobItem) {
+      return category.includes(jobItem.categoryId);
     });
   }
-  html += /*html*/`
-<div class="card">
-  <div class="card-body">
-    <div class="row">
-      <div class="col">
-        <h3 class="mb-0">${jobItem.name}</h3>
-      </div>
-      <div class="col-auto fs-3 text-green">${jobItem.salaryMin}tr - ${jobItem.salaryMax}tr</div>
-    </div>
-    <div class="row">
-    <div class="col-md">
-    <span>${company.name}</span>
-    <div class="mt-3 list-inline list-inline-dots mb-0 text-secondary d-sm-block d-none">
-          <div class="list-inline-item">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-              fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-              stroke-linejoin="round" class="icon icon-inline icon-2">
-              <path
-                d="M8 9l5 5v7h-5v-4m0 4h-5v-7l5 -5m1 1v-6a1 1 0 0 1 1 -1h10a1 1 0 0 1 1 1v17h-8" />
-              <path d="M13 7l0 .01" />
-              <path d="M17 7l0 .01" />
-              <path d="M17 11l0 .01" />
-              <path d="M17 15l0 .01" />
-            </svg>
-            ${category.name}
-          </div>
-          <div class="list-inline-item">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-              fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-              stroke-linejoin="round" class="icon icon-inline icon-2">
-              <path
-                d="M15 21h-9a3 3 0 0 1 -3 -3v-1h10v2a2 2 0 0 0 4 0v-14a2 2 0 1 1 2 2h-2m2 -4h-11a3 3 0 0 0 -3 3v11" />
-              <path d="M9 7l4 0" />
-              <path d="M9 11l4 0" />
-            </svg>
-            ${workType.name}
-          </div>
-          <div class="list-inline-item">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-              fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-              stroke-linejoin="round" class="icon icon-inline icon-2">
-              <path d="M9 11a3 3 0 1 0 6 0a3 3 0 0 0 -6 0" />
-              <path
-                d="M17.657 16.657l-4.243 4.243a2 2 0 0 1 -2.827 0l-4.244 -4.243a8 8 0 1 1 11.314 0z" />
-            </svg>
-            ${jobItem.address}
-          </div>
-        </div>
-      </div>
-      <div class="col-md-auto">
-        <div class="mt-3 badges">
-          ${htmlRequest}
-        </div>
-      </div>
-    </div>
-  </div>
-</div>`;
-});
-elJobList.innerHTML = html;
+  renderList(arrSearch);
 
-
-
+})
 // const minInput = 9;
 // const maxInput = 12;
 // const testArr = [
